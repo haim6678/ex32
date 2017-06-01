10f02ed49a6b509e60cbec48f4005bea2a5a678a
Haim Rubinstein <Haim6678@gmail.com>
2017-06-01 14:27:33 +0300

    x

---
+++ main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+
+#define ROW_SIZE 8
+#define COL_SIZE 8
+
+struct Point {
+    int x;
+    int y;
+};
+
+
+int gameBoard[ROW_SIZE][COL_SIZE];
+int main() {
+    printf("Hello, World!\n");
+    return 0;
+}
